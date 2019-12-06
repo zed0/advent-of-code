12d71292e8ce7b277a868b6dc6bc1cecb8ad6b0b
Ben Falconer <ben@falconers.me.uk>
2019-12-06 01:38:21 +0000

    Add test for intcode

--- 2019/intcode/src/lib.rs
+++ 2019/intcode/src/lib.rs
@@ -192,8 +192,19 @@ impl VirtualMachine {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn day_5_example() {
+        let code = vec![
+            3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+            1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+            999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+        ];
+
+        let mut v = VirtualMachine::new(code);
+        let mut inputs = vec![8].into();
+        v.set_inputs(&mut inputs);
+        v.run();
+        assert_eq!(v.get_outputs(), &vec![1000_i64]);
     }
 }

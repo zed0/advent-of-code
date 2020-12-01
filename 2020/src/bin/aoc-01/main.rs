01e0df2a7eb3e3cc8a6774d44ad276fcadd0b969
Ben Falconer <ben@falconers.me.uk>
2020-12-01 09:43:02 +0000

    Add 2020/01

---
+++ 2020/src/bin/aoc-01/main.rs
@@ -0,0 +1,54 @@
+use std::fs;
+use std::env;
+use std::str::FromStr;
+use itertools::Itertools;
+
+fn find_sums(nums: &Vec<u64>, target: u64, size: usize) -> Vec<Vec<u64>> {
+    nums.iter()
+        .cloned()
+        .combinations(size)
+        .filter(|i| i.iter().cloned().sum::<u64>() == target)
+        .collect_vec()
+}
+
+fn print_results(nums: &Vec<Vec<u64>>) {
+    nums.iter()
+        .for_each(|r| {
+            println!("Numbers: {:?}", r);
+            println!("Product: {:?}", r.iter().cloned().fold1(|x,y| x*y).unwrap());
+        });
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let nums: Vec<u64> = fs::read_to_string(&args[1])
+        .expect("Could not open input")
+        .lines()
+        .map(|i| i.to_string())
+        .map(|i| u64::from_str(&i).unwrap())
+        .collect();
+
+    let target = 2020;
+    println!("Part 1");
+    print_results(&find_sums(&nums, target, 2));
+    println!("Part 2");
+    print_results(&find_sums(&nums, target, 3));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_sums;
+
+    #[test]
+    fn example1() {
+        let nums: Vec<u64> = vec![
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456,
+        ];
+        assert_eq!(find_sums(&nums, 2020, 2), vec![[1721, 299]]);
+    }
+}

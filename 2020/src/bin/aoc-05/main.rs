684a558d450d47ec3f774b510bdbd8dc156f39bf
Ben Falconer <ben@falconers.me.uk>
2020-12-05 11:08:54 +0000

    Minor optimisations for 2020/05

--- 2020/src/bin/aoc-05/main.rs
+++ 2020/src/bin/aoc-05/main.rs
@@ -2,16 +2,20 @@ use std::fs;
 use std::env;
 use std::time::SystemTime;
 
-fn seat_to_id(seat: &str) -> u64 {
+fn seat_to_id(seat: &str) -> u32 {
     let (row, col) = seat.split_at(7);
-        8 * u64::from_str_radix(&row.replace("F", "0").replace("B", "1"), 2).unwrap()
-            + u64::from_str_radix(&col.replace("L", "0").replace("R", "1"), 2).unwrap()
+        8 * u32::from_str_radix(&row, 2).unwrap()
+            + u32::from_str_radix(&col, 2).unwrap()
 }
 
-fn find_missing(seat_ids: &Vec<u64>) -> u64 {
+fn input_to_binary(input: &str) -> String {
+    input.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1")
+}
+
+fn find_missing(seat_ids: &Vec<u32>) -> u32 {
     let before_min = seat_ids.iter().min().unwrap() - 1;
     let max = seat_ids.iter().max().unwrap();
-    let sum: u64 = seat_ids.iter().sum();
+    let sum: u32 = seat_ids.iter().sum();
     let expected_sum = (max * (max+1))/2 - ((before_min * (before_min+1))/2);
     return expected_sum - sum;
 }
@@ -23,11 +27,13 @@ fn main() {
     let input = fs::read_to_string(&args[1])
         .expect("Could not open input");
 
-    let ids: Vec<u64> = input.lines()
+    let setup_time = SystemTime::now();
+
+
+    let binary_input = input_to_binary(&input);
+    let ids: Vec<u32> = binary_input.lines()
         .map(|line| seat_to_id(line))
         .collect();
-
-    let setup_time = SystemTime::now();
     let part_1_ans = ids.iter().max().unwrap();
     let part_1_time = SystemTime::now();
     let part_2_ans = find_missing(&ids);
@@ -45,6 +51,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::seat_to_id;
+    use super::input_to_binary;
 
     fn example1() -> String {
         String::from("FBFBBFFRLR")
@@ -52,7 +59,7 @@ mod tests {
 
     #[test]
     fn example1a() {
-        let seat = &example1();
+        let seat = input_to_binary(&example1());
         assert_eq!(seat_to_id(&seat), 357);
     }
 
@@ -66,8 +73,8 @@ BBFFBBFRLL"
 
     #[test]
     fn example2a() {
-        let input = &example2();
-        let ids: Vec<u64> = input.lines()
+        let binary_input = input_to_binary(&example2());
+        let ids: Vec<u32> = binary_input.lines()
             .map(|line| seat_to_id(line))
             .collect();
         assert_eq!(ids.iter().max().unwrap(), &820);
